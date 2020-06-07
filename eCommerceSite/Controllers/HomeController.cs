ff0f76cfaedef2adacf49d79024f0ceb201c7f00
Mohsin Khan Shuvo <mkshuvo@hotmail.com>
2020-06-07 16:56:06 +0600

    New template, category at home, Modal template on product title

--- eCommerceSite/Controllers/HomeController.cs
+++ eCommerceSite/Controllers/HomeController.cs
@@ -6,22 +6,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using eCommerceSite.Models;
-
+using Microsoft.EntityFrameworkCore;
+using eCommerceSite.Models.Cetagory;
 
 namespace eCommerceSite.Controllers
 {
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DatabaseContext _context;
+
+        public HomeController( ILogger<HomeController> logger, DatabaseContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var category = await _context.Cetagorie.Select(x => new { x.Title, x.Photo }).ToListAsync();
+            List<HomeCategoryViewModel> HomeCategory = new List<HomeCategoryViewModel>();
+            if (category.Count == 0)
+            {
+                return View(HomeCategory);
+            }
+            for (int i = 0; i <category.Count; i++)
+            {
+                var CategoryList = new HomeCategoryViewModel()
+                {
+                    Category_Title = category[i].Title,
+                    Category_Image = category[i].Photo
+                };
+                HomeCategory.Add(CategoryList);
+            }
+            return View(HomeCategory);
         }
 
         public IActionResult Privacy()
---
+++ eCommerceSite/Models/Cetagory/HomeCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models.Cetagory
+{
+    [NotMapped]
+    public class HomeCategoryViewModel
+    {
+        public string Category_Title { get; set; }
+        public string Category_Image { get; set; }
+
+    }
+}
